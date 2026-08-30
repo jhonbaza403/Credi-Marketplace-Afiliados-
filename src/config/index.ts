764dfec633eae50b16ba88bd85cc435ca81f0223
Jhon baza <bazwjhon@gmail.com>
2026-08-30 15:11:10 -0400

    Add files via upload

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+// ==========================================================
+// CREDI MARKETPLACE
+// Global Configuration Export
+// ==========================================================
+
+
+export * from './app.config';
+
+export * from './security.config';
+
+export * from './payment.config';
+
+export * from './ai.config';
+
+export * from './database.config';
+
+export * from './routes.config';
