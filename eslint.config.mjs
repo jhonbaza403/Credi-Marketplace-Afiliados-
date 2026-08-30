e9a264a963f54384ed935965bdfb6e478aa391fb
Jhon baza <bazwjhon@gmail.com>
2026-08-30 06:07:05 -0400

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import next from "eslint-config-next";
+
+export default [
+  ...next,
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "coverage/**",
+      "playwright-report/**",
+      "test-results/**"
+    ]
+  }
+];
