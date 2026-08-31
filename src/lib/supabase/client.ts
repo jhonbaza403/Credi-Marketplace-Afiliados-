965f110a81621e18b69aa90b551f413846e4881f
Jhon baza <bazwjhon@gmail.com>
2026-08-31 19:37:42 -0400

    Simplify createClient by using non-null assertions
    Refactor createClient function to use non-null assertion for environment variables.

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,12 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
-
-  if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Faltan las variables de entorno de Supabase en el cliente.')
-  }
-
-  return createBrowserClient(supabaseUrl, supabaseKey)
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  )
 }
