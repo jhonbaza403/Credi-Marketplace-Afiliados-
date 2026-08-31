8d6f2cb411b1de132e1feb09e562e1841f1b8555
Jhon baza <bazwjhon@gmail.com>
2026-08-31 09:09:07 -0400

    Delete home.spec.ts

--- home.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("home page responds successfully", async ({ page }) => {
-  const response = await page.goto("/");
-  expect(response?.ok()).toBeTruthy();
-  await expect(page).toHaveTitle(/Credi Marketplace/i);
-});
