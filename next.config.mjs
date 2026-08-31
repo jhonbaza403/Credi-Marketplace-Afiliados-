d6462eb441a9a351b20448dd475cbdcafe6d1412
Jhon baza <bazwjhon@gmail.com>
2026-08-31 17:11:18 -0400

    Refactor next.config.mjs to use PowerShell commands

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,2 @@
-```javascript id="j6q8pu"
-const nextConfig = {};
-
-export default nextConfig;
-```
+Set-Content -Path .\next.config.mjs -Value 'const nextConfig = {};' -Encoding utf8
+Add-Content -Path .\next.config.mjs -Value 'export default nextConfig;'
