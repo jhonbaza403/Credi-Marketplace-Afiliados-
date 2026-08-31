96b8f037b17b392fef748258d95df6ac740354e2
Jhon baza <bazwjhon@gmail.com>
2026-08-31 15:48:15 -0400

    Update next-env.d.ts with TypeScript references
    Added TypeScript references for Next.js types.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
+```typescript
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited manually.
 // It is generated and maintained by Next.js.
+```
