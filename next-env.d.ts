1e2b76f081bc6a9cb67da4315797b1d35b7678e4
Jhon baza <bazwjhon@gmail.com>
2026-09-02 07:34:03 -0400

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,7 +0,0 @@
-```typescript
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited manually.
-// It is generated and maintained by Next.js.
-```
