dcb0ff51019f408341acf939c4e5401dfba57856
Jhon baza <bazwjhon@gmail.com>
2026-09-02 15:20:41 -0400

    Refactor use-auth.ts for improved readability
    Refactor import statements and format code for consistency.

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,10 +1,16 @@
 "use client";
 
-import { useContext } from "react";
-import { AuthContext } from "@/context/AuthContext";
+import {
+  useContext,
+} from "react";
+
+import {
+  AuthContext,
+} from "@/context/AuthContext";
 
 export function useAuth() {
-  const context = useContext(AuthContext);
+  const context =
+    useContext(AuthContext);
 
   if (!context) {
     throw new Error(
