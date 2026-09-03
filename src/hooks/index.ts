8ce67c4fb16ca461de3a36fd623cb65b0c26f2d8
Jhon baza <bazwjhon@gmail.com>
2026-09-03 11:40:12 -0400

    refactor: add hooks public barrel

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+// Shared client hooks.
+export { useAuth } from "./use-auth";
+export { useCart } from "./use-cart";
+export { useProducts } from "./use-products";
+export { useUser } from "./use-user";
