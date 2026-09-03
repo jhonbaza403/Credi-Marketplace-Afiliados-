4528cf54b74dcd45a58ce70bbbcfb86c7335d1db
Jhon baza <bazwjhon@gmail.com>
2026-09-03 11:41:30 -0400

    refactor: consolidate cart hook implementation

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -1,22 +1,7 @@
 "use client";
 
-import {
-  useContext,
-} from "react";
-
-import {
-  CartContext,
-} from "@/context/CartContext";
-
-export function useCart() {
-  const context =
-    useContext(CartContext);
-
-  if (!context) {
-    throw new Error(
-      "useCart debe utilizarse dentro de CartProvider",
-    );
-  }
-
-  return context;
-}
+/**
+ * Compatibility facade.
+ * The canonical cart hook lives with CartContext.
+ */
+export { useCart } from "@/context/CartContext";
