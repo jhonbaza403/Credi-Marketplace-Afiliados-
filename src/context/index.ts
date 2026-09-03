8ba79002c4fb5297e372dc76223105d948a72061
Jhon baza <bazwjhon@gmail.com>
2026-09-03 11:40:08 -0400

    refactor: add context public barrel

---
+++ src/context/index.ts
@@ -0,0 +1,5 @@
+// Global React context exports.
+export { AuthProvider, useAuth } from "./AuthContext";
+export { CartProvider, useCart } from "./CartContext";
+export { LanguageProvider, useLanguage } from "./LanguageContext";
+export { RegionProvider, useRegion } from "./RegionContext";
