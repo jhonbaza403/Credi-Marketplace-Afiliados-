76ebbe18d2039c6b101d51bcd95ed529e6a9abbe
Jhon baza <bazwjhon@gmail.com>
2026-09-03 11:42:11 -0400

    refactor: unify pagination constants

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,2 +1,4 @@
-export const DEFAULT_PAGE_SIZE = 20;
-export const MAX_PAGE_SIZE = 100;
+import { APP_CONSTANTS } from "@/config/constants";
+
+export const DEFAULT_PAGE_SIZE = APP_CONSTANTS.pagination.defaultLimit;
+export const MAX_PAGE_SIZE = APP_CONSTANTS.pagination.maxLimit;
